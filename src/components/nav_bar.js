7732344daaa5a495bd6489e4dee5b473cc47d59a
huynq <qhuydtvt@gmail.com>
2017-08-20 06:12:41 +0700

    Changed nav bar background color

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -15,10 +15,10 @@ class NavBar extends Component {
 
   render() {
       return (
-          <nav className="row light-blue darken-4">
+          <nav className="row teal darken-1">
               <div className="container">
-                <div className="col s6 search-wrapper card focused light-blue">
-                  <input id="search" onChange={this.handleSearch} className="flow-text input-search light-blue" />
+                <div className="col s6 search-wrapper card focused teal lighten-4">
+                  <input id="search" onChange={this.handleSearch} className="flow-text input-search" />
                 </div>
               </div>
           </nav>
