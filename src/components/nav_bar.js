c7762e2ce72da6a8d7881cbae533038d024639fe
huynq <qhuydtvt@gmail.com>
2017-08-20 02:07:13 +0700

    Made search bar smaller

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class NavBar extends Component {
   render() {
       return (
-          <nav className="light-blue darken-4">
-              <div className="row container">
-                <div className="col s12 search-wrapper card focused light-blue">
+          <nav className="row light-blue darken-4">
+              <div className="container">
+                <div className="col s6 search-wrapper card focused light-blue">
                   <input id="search" className="flow-text input-search light-blue" />
                 </div>
               </div>
